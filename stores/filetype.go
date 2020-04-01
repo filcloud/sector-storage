8a020a14dc2c05ae25d283a387b38b79f791313b
RideWindX <ridewindx@163.com>
2020-04-01 18:28:54 +0800

    Add merkle tree proof callback

--- stores/filetype.go
+++ stores/filetype.go
@@ -11,6 +11,7 @@ const (
 	FTUnsealed SectorFileType = 1 << iota
 	FTSealed
 	FTCache
+	FTMeta
 
 	FileTypes = iota
 )
@@ -43,6 +44,8 @@ func (t SectorFileType) String() string {
 		return "sealed"
 	case FTCache:
 		return "cache"
+	case FTMeta:
+		return "meta"
 	default:
 		return fmt.Sprintf("<unknown %d>", t)
 	}
